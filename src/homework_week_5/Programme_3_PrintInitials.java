2a91dd471298d30343474e755cfe0af1911161a0
Komal Vachhani <komal129@gmail.com>
2022-10-14 14:42:56 +0100

    My week5 writing homework

---
+++ src/homework_week_5/Programme_3_PrintInitials.java
@@ -0,0 +1,17 @@
+package homework_week_5;
+
+public class Programme_3_PrintInitials {
+
+    public static void main(String[] args) {
+
+        System.out.println("*********  ********  *********  ********");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   ********        **   ********");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("   ****    **    **     ****    **    **");
+        System.out.println("    **     **    **      **     **    **");
+    }
+}
---
+++ src/homework_week_5/Programme_4_RightTriangle.java
@@ -0,0 +1,14 @@
+package homework_week_5;
+
+public class Programme_4_RightTriangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
